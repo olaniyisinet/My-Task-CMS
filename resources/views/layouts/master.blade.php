71f25fc3ed4087a4600c95b45e8a52cd7dfabeec
Olaniyi Olatunji <olaniyiolatunji@gmail.com>
2019-05-07 11:05:05 +0200

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -51,9 +51,9 @@
                      <li class="nav-item">
                          <a class="nav-link" href="{{ route('login') }}">Login</a>
                     </li>
-                     <li class="nav-item">
-                         <a class="nav-link" href="{{ route('register') }}">Register</a>
-                         </li>
+{{--                     <li class="nav-item">--}}
+{{--                         <a class="nav-link" href="{{ route('register') }}">Register</a>--}}
+{{--                         </li>--}}
                      @endauth
              @endif
           </ul>
